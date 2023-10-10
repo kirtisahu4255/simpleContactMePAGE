dc7f14a717db55bc612826cd8b0338023c4f653a
kirti sahu <94732027+kirtisahu4255@users.noreply.github.com>
2023-10-10 19:24:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents the form from submitting the traditional way
+    
+    // You can add your form submission logic here if you're using JavaScript on the client side
+    
+    // Show a confirmation message
+    alert('Thank you for your message! We will get back to you soon.');
+});
